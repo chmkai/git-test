83fe67b7cde32c9e1db5fc6795c1a4f9a0cbbba1
Kolkonut <daanishbaig@hotmail.com>
2017-05-12 13:25:58 +0300

    Create SimpleCode.cpp
    We store the dev code here.

---
+++ SimpleCode.cpp
@@ -0,0 +1,16 @@
+//This is where the code for our program will be written.
+//This is basic for now.
+//Make new issues for each modification. This is the main dev.
+
+#include <iostream.h>
+#include <conio.h>
+#include <fstream.h>
+#include <stdio.h>
+
+//Need to make functions.
+
+void main()
+{
+  cout<<"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget nibh vitae lorem scelerisque aliquam quis a metus. Sed et."
+  getch();
+}
